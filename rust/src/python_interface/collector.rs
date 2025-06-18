92f94e3a945d85f6f95b3efdccd7fa9a48c90902
victor-villar <villar@ibm.com>
2025-06-18 12:28:59 -0400

    Missing licences
    Signed-off-by: victor-villar <villar@ibm.com>

--- rust/src/python_interface/collector.rs
+++ rust/src/python_interface/collector.rs
@@ -1,4 +1,16 @@
-// Now create a Python-facing proxy struct using PyO3
+// -*- coding: utf-8 -*-
+/* 
+(C) Copyright 2025 IBM. All Rights Reserved.
+
+This code is licensed under the Apache License, Version 2.0. You may
+obtain a copy of this license in the LICENSE.txt file in the root directory
+of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+
+Any modifications or derivative works of this code must retain this
+copyright notice, and modified files need to carry a notice indicating
+that they have been altered from the originals.
+*/
+
 use pyo3::prelude::*;
 use std::collections::HashMap;
 
